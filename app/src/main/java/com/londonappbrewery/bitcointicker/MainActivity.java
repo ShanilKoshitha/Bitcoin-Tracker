fcda9c549a82449e11ed176f53006b7b181a2638
Shanil Koshitha <shanilkoshitha@rocketmail.com>
2018-11-24 19:57:34 -0400

    commit

---
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -0,0 +1,101 @@
+package com.londonappbrewery.bitcointicker;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Constants:
+    // TODO: Create the base URL
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
+
+
+    // Member Variables:
+    TextView mPriceTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPriceTextView = findViewById(R.id.priceLabel);
+        Spinner spinner = findViewById(R.id.currency_spinner);
+
+        // Create an ArrayAdapter using the String array and a spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.currency_array, R.layout.spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+               String itemSelected = parent.getItemAtPosition(position).toString();
+               Log.d("bitcoin", "Item selected is: " + itemSelected);
+               letsDoSomeNetworking(itemSelected);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d("bitcoin", "nothing was selected");
+            }
+        });
+    }
+
+    // TODO: complete the letsDoSomeNetworking() method
+    private void letsDoSomeNetworking(String url) {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(BASE_URL+url, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+                Log.d("bitcoin", "JSON: " + response.toString());
+                BitCoinDataModel coinData = BitCoinDataModel.fromJson(response);
+                Log.d("bitcoin", "the price is: "+coinData);
+
+                mPriceTextView.setText(""+coinData.getPrice());
+                //updateUI(weatherData);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d("bitcoin", "Request fail! Status code: " + statusCode);
+                Log.d("bitcoin", "Fail response: " + response);
+                Log.e("bitcoin", e.toString());
+                Toast.makeText(MainActivity.this, "Request Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+}
